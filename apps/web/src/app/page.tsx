78a6e69f412937adf7045acb1411b2fa9ad87cd8
Ítalo Rezende <italo.rezende@sharkdev.com.br>
2024-03-07 21:47:58 -0300

    test: Testando ci/cd

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,7 +2,9 @@ import Image from "next/image";
 import { trpc } from "./trpc";
 
 export default async function Home() {
-  const response = await trpc.hello.query();
+  const response = await trpc.hello.query({
+    name: "Ítalo",
+  });
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
